5ea0a11ca64cc355b82502d0b0d08181f7f35d42
teeeeeeemo <binooooo@naver.com>
2017-02-22 16:42:56 +0900

    navigation

---
+++ Navigation/EditViewController.swift
@@ -0,0 +1,75 @@
+//
+//  EditViewController.swift
+//  Navigation
+//
+//  Created by Lucia on 2017. 2. 22..
+//  Copyright © 2017년 Lucia. All rights reserved.
+//
+
+import UIKit
+
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+}
+
+class EditViewController: UIViewController {
+    
+    var textWayValue: String = ""
+    var textMessage: String = ""
+    var isOn = false
+    var delegate: EditDelegate?
+
+    @IBOutlet weak var txMessage: UITextField!
+    @IBOutlet weak var lblWay: UILabel!
+    @IBOutlet weak var swIsOn: UISwitch!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swIsOn.isOn = isOn
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // 메인화면으로 텍스트와 전구 이미지 상태 보내기.
+    @IBAction func btnDone(_ sender: UIButton) {
+        
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+        }
+        
+        
+        // 뷰를 전환하기 위해 세그웨이 추가시 [Action Segue]를 'Show'로 지정.
+        // 따라서 되돌아갈 때는 'pop' 형태로 해줘야~~~
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    // 전구 켜고 끄기
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, EditDelegate {
+    
+    let imgOn = UIImage(named: "lamp-on.png")
+    let imgOff = UIImage(named: "lamp-off.png")
+    
+    var isOn = true
+    
+    @IBOutlet weak var txMessage: UITextField!
+    @IBOutlet weak var imgView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgView.image = imgOn
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 세그웨이를 이용한 화면 전환
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as!
+                    EditViewController
+        if segue.identifier == "editButton" {
+            // 버튼 클릭
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            // 바 버튼 클릭
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+        editViewController.textMessage = txMessage.text!
+        editViewController.isOn = isOn 
+        editViewController.delegate = self
+    }
+    
+    
+    // 메시지 값을 텍스트 필드에 표시
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
+    }
 
+    // 전구 이미지 값 세팅
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
 
 }
 
